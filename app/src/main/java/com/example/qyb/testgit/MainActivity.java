68e60b24de989e428be316590ffdd375dbe49378
YuboQiu <634966673@qq.com>
2017-01-10 11:52:58 +0800

    add a lot of files.

---
+++ app/src/main/java/com/example/qyb/testgit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.qyb.testgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String TAG="TestGit";
+    public static String TAG2="TestGit2";
+    public static String TAG3="TestGit3";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
